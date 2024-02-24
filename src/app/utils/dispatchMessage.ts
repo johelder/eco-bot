a0b81861c442acd257c69bdc1e79163e8ca43d6b
johelder <johelder.arj@gmail.com>
2024-02-24 10:37:28 -0300

    refactor: reply message

--- src/app/utils/dispatchMessage.ts
+++ src/app/utils/dispatchMessage.ts
@@ -1,10 +1,9 @@
 import { Message } from 'whatsapp-web.js';
-import { client } from '../server';
 
 const TWO_SECONDS = 2000;
 
 export async function dispatchMessage(message: Message, text: string) {
   return setTimeout(() => {
-    client.sendMessage(message.from, text);
+    message.reply(text);
   }, TWO_SECONDS);
 }
